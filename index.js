6603736048060ba4cd4e4cb720933dae87d9a187
ifti2021331087 <ishmamiftinoor3@gmail.com>
2026-03-18 06:40:39 +0600

    new changes avaiable

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.port || 5001;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dxoja8w.mongodb.net/?appName=Cluster0`;
 
@@ -29,42 +32,96 @@ async function run() {
 
     const userCollection = client.db("vibe").collection("users");
     const workCollection = client.db("vibe").collection("works");
+    const paymentCollection = client.db("vibe").collection("payments");
 
 
+    // jwt related apis
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({ token });
+    })
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("Authorization Header:", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+      const admin = user?.role === 'admin';
+      if (!admin) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      next();
+    }
+    const verifyHR = async (req, res, next) => {
+      const email = req.decoded.email;
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+      const hr = user?.role === 'HR';
+      if (!hr) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      next();
+    }
+    const verifyAdminOrHR = async (req, res, next) => {
+      const email = req.decoded.email;
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+      if (user?.role === 'admin' || user?.role === 'HR') {
+        return next();
+      }
+      return res.status(403).send({ message: 'Forbidden access' });
+    }
 
 
     // user related api
-    app.get('/users',async(req,res)=>{
-      const email=req.query.email;
-      const query={email:email};
-      const result=await userCollection.findOne(query);
-      if(result){
+    app.get('/users', verifyToken, verifyAdminOrHR, async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      if (result && email) {
         res.send(result);
       }
-      else{
-        const allUser=await userCollection.find().toArray();
+      else {
+        const allUser = await userCollection.find().toArray();
         res.send(allUser);
       }
     })
-    app.get('/users/hr',async(req,res)=>{
-      const email=req.query.email;
-      const filter={email:email};
-      const user=await userCollection.findOne(filter);
-      let hr=false;
-      if(user){
-        hr=user?.role==='HR';
+    app.get('/users/hr',async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+      let hr = false;
+      if (user) {
+        hr = user?.role === 'HR';
       }
-      res.send({hr});
+      res.send({ hr });
     })
-    app.get('/users/admin',async(req,res)=>{
-      const email=req.query.email;
-      const filter={email:email};
-      const user=await userCollection.findOne(filter);
-      let admin=false;
-      if(user){
-        admin=user?.role==='admin';
+    app.get('/users/admin',async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
       }
-      res.send({admin});
+      res.send({ admin });
     })
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -77,38 +134,130 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/users', async (req, res) => {
-      const user=req.body;
+    app.patch('/users', verifyToken, async (req, res) => {
       const email = req.query.email;
-      const filter = { email:email };
+      const filter = { email: email };
+      const updateData = req.body;
       const updatedDoc = {
-        $set: {
-          bankAccountNo:user.bankAccountNo,
-          designation:user.designation,
-          salary:user.salary,
-        }
+        // $set: {
+        //   role: user.role,
+        //   bankAccountNo:user.bankAccountNo,
+        //   designation:user.designation,
+        //   salary:user.salary,
+        //   isVarified:user.isVarified,
+        //   isFired:user.isFired,
+        // }
+        $set: updateData
       }
-      const result=await userCollection.updateOne(filter,updatedDoc);
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
 
 
     // work-sheet related apis
-    app.get('/works',async(req,res)=>{
-      const email=req.query.email;
-      const filter={email:email};
-      const filtered=await workCollection.findOne(filter);
-      if(filtered){
+    app.get('/works', verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const filtered = await workCollection.find(filter).toArray();
+      if (filtered) {
         res.send(filtered);
       }
-      else{
-        const allWorks=await workCollection.find().toArray();
+      else {
+        const allWorks = await workCollection.find().toArray();
         res.send(allWorks);
       }
     })
-    app.post('/works',async(req,res)=>{
-      const work=req.body;
-      const result=await workCollection.insertOne(work);
+    app.post('/works', verifyToken, async (req, res) => {
+      const work = req.body;
+      const result = await workCollection.insertOne(work);
+      res.send(result);
+    })
+
+    // hr-related apis
+    app.get('/payroll', verifyToken, verifyAdminOrHR, async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const filterdPayments = await paymentCollection.find(filter).toArray();
+      if (filterdPayments && email) {
+        res.send(filterdPayments);
+      }
+      else {
+        const allPayments = await paymentCollection.find().toArray();
+        res.send(allPayments);
+      }
+    })
+    app.post('/payroll', verifyToken, verifyAdminOrHR, async (req, res) => {
+      const payment = req.body;
+      const result = paymentCollection.insertOne(payment);
+      res.send(result);
+    })
+
+
+    // admin-payment-related
+    app.post('/create-checkout-session', async (req, res) => {
+      const { id, name, email, salary, month, year } = req.body;
+      // console.log(id);
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: 'payment',
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: `Salary for ${name}`,
+                description: `Payment for ${month} , ${year}`,
+              },
+              unit_amount: Math.round(salary * 100)
+            },
+            quantity: 1,
+          },
+        ],
+        metadata: {
+          id: id,
+          email: email,
+          month: month,
+          year: year
+        },
+        success_url: `http://localhost:5173/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}&userId=${id}`,
+        cancel_url: "http://localhost:5173/dashboard/payment-cancel",
+      });
+      // console.log(session.id);
+      // console.log(CHECKOUT_SESSION_ID);
+      res.json({
+        id: session.id,
+        url: session.url
+      })
+    })
+
+    app.get('/payment-info/:sessionId', async (req, res) => {
+      try {
+        const session = await stripe.checkout.sessions.retrieve(req.params.sessionId);
+        res.send({
+          transactionId: session.payment_intent, // This is the 'pi_...' ID
+          paymentStatus: session.payment_status,
+          customerEmail: session.metadata.id,
+          customerEmail: session.metadata.email
+        });
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    })
+
+    app.get('/userSalary/:id', verifyToken, verifyAdminOrHR, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await paymentCollection.findOne(filter);
+      res.send(result);
+    })
+    app.patch('/userSalary/:id', verifyToken, verifyAdminOrHR, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const info = req.body;
+      const updatedDoc = {
+        $set: info
+      }
+      const result = await paymentCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
 
