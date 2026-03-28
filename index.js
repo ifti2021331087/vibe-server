cf3e2fc0ec3d0c95f0222f082813e106be314ff2
ifti2021331087 <ishmamiftinoor3@gmail.com>
2026-03-28 19:29:18 +0600

    new

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const verifyToken = (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     app.get('/', (req, res) => {
       res.send('Server is running!');
     });
